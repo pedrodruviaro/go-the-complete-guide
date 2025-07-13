701e8bbcdb3195d96da5f8b7afc094743009045f
pedrodruviaro <pedrodruviaro@gmail.com>
2025-07-13 15:08:12 -0300

    init repo

---
+++ investment_calculator.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+func main() {
+	// var investmentAmount float64 = 1000
+	// var years float64 = 10
+	var investmentAmount, years float64 = 1000, 10
+	expectedReturnRate := 5.5
+
+	futureValue := investmentAmount *  math.Pow(1 + expectedReturnRate / 100, years)
+
+	fmt.Println(futureValue)
+}
